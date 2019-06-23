959f163d073eb31657a2b453c8aaead4d0c6d82e
Mohammed Asker <zetoanime@gmail.com>
2019-06-23 11:32:23 +0300

    files created

---
+++ main.js
@@ -0,0 +1,8 @@
+// Create a new note
+
+	document.getElementById('btnNew').addEventListener("click", function() {
+	document.getElementById('newNote').innerHTML = 'it works';
+});
+
+// Edit the note
+// Delete the note
