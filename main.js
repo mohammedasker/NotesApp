07b9d84947a31f02731602edb14b2885690d4525
Mohammed Asker <zetoanime@gmail.com>
2019-09-13 11:35:50 +0300

    text is saved and shows as a list when clicked save button

--- main.js
+++ main.js
@@ -5,6 +5,20 @@
 });
 
 // SAVE THE NOTE
+// Save a note when clicked the save button
+function newElement() {
+	const li = document.createElement("li");
+	const inputvalue = document.querySelector("#myInput").value;
+	const text = document.createTextNode(inputvalue);
+	li.appendChild(text);
+	if (inputvalue === ''){
+		alert("Please write something")
+	} else {
+		document.querySelector("#myNotes").appendChild(li);
+	}
+	document.querySelector("#myInput").value = '';
+}
+
 
 // EDIT THE NOTE
 
