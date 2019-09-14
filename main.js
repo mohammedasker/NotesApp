0f2ebd6917101d2e8d854670251de233f7f3986f
Mohammed Asker <zetoanime@gmail.com>
2019-09-14 18:12:19 +0300

    add delete codes but not working yet

--- main.js
+++ main.js
@@ -7,9 +7,9 @@
 // SAVE THE NOTE
 // Save a note when clicked the save button
 function newElement() {
-	const li = document.createElement("li");
-	const inputvalue = document.querySelector("#myInput").value;
-	const text = document.createTextNode(inputvalue);
+	let li = document.createElement("li");
+	let inputvalue = document.querySelector("#myInput").value;
+	let text = document.createTextNode(inputvalue);
 	li.appendChild(text);
 	if (inputvalue === ''){
 		alert("Please write something")
@@ -19,7 +19,21 @@ function newElement() {
 	document.querySelector("#myInput").value = '';
 }
 
+// DELETE THE NOTE
+// Clear inputs on the text area
+function clearElement() {
+	document.querySelector("#myInput").value = '';
+}
+
+// Delete the saved note
+let nodeList = document.getElementsByTagName("LI");
+let i;
+for (i = 0; i < nodeList.length; i++){
+	let span = document.createElement("SPAN");
+	let txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild("txt");
+	nodeList[i].appendChild(span);
+}
 
 // EDIT THE NOTE
-
-// DELETE THE NOTE
