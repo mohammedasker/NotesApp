a9f7d93102f77dfedf84c8067b25bb6d88e4854c
Mohammed Asker <zetoanime@gmail.com>
2019-09-15 18:21:22 +0300

    delete button spans when notes are saved

--- main.js
+++ main.js
@@ -5,19 +5,6 @@
 });
 
 // SAVE THE NOTE
-// Save a note when clicked the save button
-function newElement() {
-	let li = document.createElement("li");
-	let inputvalue = document.querySelector("#myInput").value;
-	let text = document.createTextNode(inputvalue);
-	li.appendChild(text);
-	if (inputvalue === ''){
-		alert("Please write something")
-	} else {
-		document.querySelector("#myNotes").appendChild(li);
-	}
-	document.querySelector("#myInput").value = '';
-}
 
 // DELETE THE NOTE
 // Clear inputs on the text area
@@ -30,10 +17,47 @@ let nodeList = document.getElementsByTagName("LI");
 let i;
 for (i = 0; i < nodeList.length; i++){
 	let span = document.createElement("SPAN");
-	let txt = document.createTextNode("\u00D7");
+	let text = document.createTextNode("\u00D7");
 	span.className = "close";
-	span.appendChild("txt");
+	span.appendChild(text);
 	nodeList[i].appendChild(span);
+}
+
+// Click the close button to hide saved notes
+let close = document.getElementsByClassName("close");
+    i;
+for (i = 0; i < close.length; i++){
+	close[i].close = function() {
+	let div = this.parentElement;
+	div.style.display = "none";
+	}
+}
+
+// Save a note when clicked the save button
+function newElement() {
+	let li = document.createElement("li");
+	let inputvalue = document.querySelector("#myInput").value;
+	let savedNote = document.createTextNode(inputvalue);
+	li.appendChild(savedNote);
+	if (inputvalue === ''){
+		alert("Please write something")
+	} else {
+		document.querySelector("#myNotes").appendChild(li);
+	}
+	document.querySelector("#myInput").value = '';
+
+	// Span close button every time new notes are saved
+	let span = document.createElement("span");
+	text = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(text);
+	li.appendChild(span);
+	for (i = 0; i < close.length; i++){
+		close[i].close = function() {
+			let div = this.parentElement;
+			div.style.display = "none";
+		}
+	}
 }
 
 // EDIT THE NOTE
